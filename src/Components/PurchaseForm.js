ad598e6d64d5bf184e8d9bddb95314eaae1d7b3a
alienpingu <samuele.bertocco@gmail.com>
2021-11-30 18:02:07 +0100

    [UI/UX]

--- src/Components/PurchaseForm.js
+++ src/Components/PurchaseForm.js
@@ -7,13 +7,18 @@ import {
         Row,
         Col,
         Button,
+        InputGroup
       } from 'react-bootstrap';
+
 
 const schema = yup.object().shape({
   firstName: yup.string().required(),
   lastName: yup.string().required(),
-  file: yup.mixed().required(),
-  terms: yup.bool().required().oneOf([true], 'terms must be accepted'),
+  username: yup.string().required(),
+  city: yup.string().required(),
+  state: yup.string().required(),
+  zip: yup.string().required(),
+  terms: yup.bool().required().oneOf([true], 'Terms must be accepted'),
 });
 
 export default function PurchaseForm() {
@@ -24,7 +29,10 @@ export default function PurchaseForm() {
       initialValues={{
         firstName: '',
         lastName: '',
-        file: null,
+        username: '',
+        city: '',
+        state: '',
+        zip: '',
         terms: false,
       }}
     >
@@ -37,56 +45,107 @@ export default function PurchaseForm() {
         isValid,
         errors,
       }) => (
-        <Form className="purchase-form" noValidate onSubmit={handleSubmit}>
+        <Form noValidate onSubmit={handleSubmit}>
           <Row className="mb-3">
-            <Form.Group
-              as={Col}
-              md="6"
-              controlId="validationFormik101"
-              className="position-relative"
-            >
-              <Form.Label>Nome</Form.Label>
+            <Form.Group as={Col} md="4" controlId="validationFormik01">
+              <Form.Label>First name</Form.Label>
               <Form.Control
                 type="text"
                 name="firstName"
                 value={values.firstName}
                 onChange={handleChange}
                 isValid={touched.firstName && !errors.firstName}
+                isInvalid={!!errors.firstName}
+
               />
-              <Form.Control.Feedback feedbackType="invalid">Inserisci qualcosa!</Form.Control.Feedback>
+              <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+              <Form.Control.Feedback type="invalid">
+                  Inserire nome
+                </Form.Control.Feedback>
             </Form.Group>
-            <Form.Group
-              as={Col}
-              md="6"
-              controlId="validationFormik102"
-              className="position-relative"
-            >
-              <Form.Label>Cognome</Form.Label>
+            <Form.Group as={Col} md="4" controlId="validationFormik02">
+              <Form.Label>Last name</Form.Label>
               <Form.Control
                 type="text"
                 name="lastName"
                 value={values.lastName}
                 onChange={handleChange}
                 isValid={touched.lastName && !errors.lastName}
+                isInvalid={!!errors.lastName}
+
               />
 
-              <Form.Control.Feedback feedbackType="invalid">Inserisci qualcosa!</Form.Control.Feedback>
+              <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+              <Form.Control.Feedback type="invalid">
+                  Inserire cognome
+                </Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group as={Col} md="4" controlId="validationFormikUsername">
+              <Form.Label>Username</Form.Label>
+              <InputGroup hasValidation>
+                <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
+                <Form.Control
+                  type="text"
+                  placeholder="Username"
+                  aria-describedby="inputGroupPrepend"
+                  name="username"
+                  value={values.username}
+                  onChange={handleChange}
+                  isInvalid={!!errors.username}
+                />
+                <Form.Control.Feedback type="invalid">
+                  {errors.username}
+                </Form.Control.Feedback>
+              </InputGroup>
             </Form.Group>
           </Row>
-          <Form.Group className="position-relative mb-3">
-            <Form.Label>File</Form.Label>
-            <Form.Control
-              type="file"
-              required
-              name="file"
-              onChange={handleChange}
-              isInvalid={!!errors.file}
-            />
-            <Form.Control.Feedback feedbackType="invalid" tooltip>
-              {errors.file}
-            </Form.Control.Feedback>
-          </Form.Group>
-          <Form.Group className="position-relative mb-3">
+          <Row className="mb-3">
+            <Form.Group as={Col} md="6" controlId="validationFormik03">
+              <Form.Label>City</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="City"
+                name="city"
+                value={values.city}
+                onChange={handleChange}
+                isInvalid={!!errors.city}
+              />
+
+              <Form.Control.Feedback type="invalid">
+                {errors.city}
+              </Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group as={Col} md="3" controlId="validationFormik04">
+              <Form.Label>State</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="State"
+                name="state"
+                value={values.state}
+                onChange={handleChange}
+                isInvalid={!!errors.state}
+              />
+              <Form.Control.Feedback type="invalid">
+                {errors.state}
+              </Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group as={Col} md="3" controlId="validationFormik05">
+              <Form.Label>Zip</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Zip"
+                name="zip"
+                value={values.zip}
+                onChange={handleChange}
+                isInvalid={!!errors.zip}
+              />
+
+              <Form.Control.Feedback type="invalid">
+                {errors.zip}
+              </Form.Control.Feedback>
+            </Form.Group>
+          </Row>
+          <Form.Group className="mb-3">
             <Form.Check
               required
               name="terms"
@@ -95,8 +154,7 @@ export default function PurchaseForm() {
               isInvalid={!!errors.terms}
               feedback={errors.terms}
               feedbackType="invalid"
-              id="validationFormik106"
-              feedbackTooltip
+              id="validationFormik0"
             />
           </Form.Group>
           <Button type="submit">Submit form</Button>
--- src/Views/LandingPage.js
+++ src/Views/LandingPage.js
@@ -2,10 +2,13 @@ import {
 		Container,
 		Row,
 		Col,
-		Button
+		Button,
+		Form
 } from 'react-bootstrap'
 
 import phoneSrc from '../assets/image/phone.png'
+import phoneSrc2 from '../assets/image/phone2s.png'
+import phoneSrc3 from '../assets/image/phone3s.png'
 
 import { Icon } from '@iconify/react';
 import securityIcon from '@iconify/icons-mdi/security';
@@ -59,8 +62,9 @@ function LandingPage() {
 							<h2>Ordina ora la tua cover</h2>
 							<p>Qr Code del tuo GreenPass sanitario inciso sulla cover per essere sempre visibile e pronto all' uso. Comodo, Semplice, Veloce!</p>
 						</Col>
-						<Col lg={6}>
-							{"Foto"}
+						<Col lg={6} className="d-flex justify-content-center">
+							<img className="product-image" src={phoneSrc3} alt="preview-cover" />
+
 						</Col>
 						<Col lg={6}>
 							<Container className="bold">	
@@ -70,19 +74,74 @@ function LandingPage() {
 							</Container>
 						</Col>
 					</Row>
-					<Row id="CoverSection">
-						<Col lg={6}>
-							{"Foto"}
-						</Col>
-						<Col lg={6}>
-							<Container>
-								<p className="banner-subtitle">GreenPass</p>
-								<h1 className="fw-bold display-3">Cover<br/> Codecover</h1>
-								<p>Codecover si occupa della riproduzione del QR-code sanitario (GreenPass) sulle cover dello smartphone. Il QR-code riprodotto mantiene le caratteristiche e le informazioni contenute nel GreenPass fornito dal sistema sanitario nazionale e, abbinato ad un documento d' identità, SOSTITUISCE la versione digitale anche se il device non dovesse funzionare ( batteria scarica - rottura dello schermo ) e la versione cartacea la cui copia potrebbe risultare sgualcita o alterata.</p>
-								<p>Realizzate su MATERIALE INALTERABILE tramite incisione laser risulta la soluzione perfetta per praticità e reperibilità. Il contrasto su fondo opaco rende più visibile il codice anche in situazione di scarsa illuminazione.</p>
-							</Container>
-						</Col>
-					</Row>
+					<Container id="CoverSection" className="py-5">
+						<Row>
+
+							<Col lg={6}>
+									<p className="banner-subtitle">GreenPass</p>
+									<h1 className="fw-bold display-3">Cover<br/> Codecover</h1>
+									<p>Codecover si occupa della riproduzione del QR-code sanitario (GreenPass) sulle cover dello smartphone. Il QR-code riprodotto mantiene le caratteristiche e le informazioni contenute nel GreenPass fornito dal sistema sanitario nazionale e, abbinato ad un documento d' identità, SOSTITUISCE la versione digitale anche se il device non dovesse funzionare ( batteria scarica - rottura dello schermo ) e la versione cartacea la cui copia potrebbe risultare sgualcita o alterata.</p>
+									<p>Realizzate su MATERIALE INALTERABILE tramite incisione laser risulta la soluzione perfetta per praticità e reperibilità. Il contrasto su fondo opaco rende più visibile il codice anche in situazione di scarsa illuminazione.</p>
+							</Col>
+							<Col lg={6}>
+								<img className="preview-image" src={phoneSrc2} alt="preview-cover2" />
+							</Col>
+						</Row>
+					</Container>
+					<div id="ChiSiamoSection" className="py-5 text-light">
+						<Container>
+							<Row>
+								<Col lg={6}></Col>
+								<Col lg={6}>
+									<p className="banner-subtitle">CodeCover</p>
+									<h1 className="fw-bold display-3">Chi siamo </h1>
+									<p>Nata da un idea di semplicità e praticità. Codecover è un brevetto italiano per l' incisione al laser sulle cover degli smartphone. Brevettato come m.d.u 202021000004157.</p>
+									<p>UTILE per usufruire dei servizi ove necessario mostrare il green pass sanitario sveltendo così le pratiche di controllo in ambiti aziendali, areoporti, ev. sportivi, ev. fieristici, etc...</p>
+								</Col>
+							</Row>
+						</Container>
+					</div>
+					<div id="ContactSection" className="py-4">
+						<Container>
+							<Row>
+								<Col lg="6">
+									<h2>CodeCover</h2>
+									<p>Contattaci per avere ulteriori informazioni</p>
+									<a href="mailto:a@a.com" >email@email.com</a>
+								</Col>
+								<Col lg="6">
+									<h2>Compila il form per contattarci</h2>
+									<Form>
+									  <Row className="mb-3">
+									    <Form.Group as={Col} controlId="formGridEmail">
+									      <Form.Label>Email</Form.Label>
+									      <Form.Control type="email" placeholder="Enter email" />
+									    </Form.Group>
+									  </Row>
+									  <Row className="mb-3">
+									    <Form.Group as={Col} controlId="formGridEmail">
+									      <Form.Label>Nome</Form.Label>
+									      <Form.Control type="text" placeholder="Mario Rossi" />
+									    </Form.Group>
+									  </Row>
+									  <Row className="mb-3">
+									    <Form.Group as={Col} controlId="formGridEmail">
+									      <Form.Label>Messaggio</Form.Label>
+									      <Form.Control type="text" placeholder="Ciao! Avevo bisogno di.." />
+									    </Form.Group>
+									  </Row>
+									  <Form.Group className="mb-3" id="formGridCheckbox">
+									    <Form.Check type="checkbox" label="Check me out" />
+									  </Form.Group>
+
+									  <Button variant="primary" type="submit">
+									    Submit
+									  </Button>
+									</Form>
+								</Col>
+							</Row>
+						</Container>
+					</div>
 				</>)
 }
 
